5728f7ecf318e12d14dea473c1c01c2dae776d19
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-08 17:39:47 +0100

    Added config file

--- config/env.js
+++ config/env.js
@@ -4,6 +4,8 @@ dotenv.config();
 export default()=>{
     return {
         JWT_SECRET: process.env.JWT_SECRET,
-
+        CLOUD_NAME: process.env.CLOUD_NAME,
+        CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+        CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
     }
 }
