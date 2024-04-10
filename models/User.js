827e90b6c94ae99fedd1b4605a5dc1bc04ab596c
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-10 20:29:14 +0100

    Revamped User model

--- models/User.js
+++ models/User.js
@@ -1,42 +1,36 @@
 import mongoose from "mongoose";
 
-const User = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        lowercase: true,
-        index: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    salt:{
+const User = new mongoose.Schema(
+  {
+    username: {
       type: String,
-      required: true
-  },
-    followers: [
-        {
-            username: {
-                type: String,
-                lowercase: true,
-                unique: true,
-                index: true
-            },
-            _id: false
-        }
-    ]
-},
-{
-  timestamps: true,
-  toJSON: {
-    transform: function (doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-      delete ret.__v;
+      required: true,
+      lowercase: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    //username
+    followers: [{username:{
+      type: String
+    }, _id: false}],
+    salt: {
+      type: String,
+      required: true,
     },
   },
-}
-); 
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 export default mongoose.model("User", User);
