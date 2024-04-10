33d37b9d348d770dd91ceadabb1d0f489b438a56
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-10 20:22:14 +0100

    Added post routes to entry point

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import express from "express";
-// import your routes
-import userRoute from "./routes/users.js"
+import mongoose from "mongoose";
+import userRoute from "./routes/users.js";
+import verifyAuth from "./middleware/verifyAuth.js";
+import postRoute from "./routes/post.js";
 const app = express();
 
 app.use(express.json());
-// use your routes
-
-app.use('/users', userRoute);
-
+app.use("/users", userRoute);
+app.use("/posts", verifyAuth , postRoute);
 
 const port = process.env.PORT ?? 5000;
-app.listen(port, ()=>{
-console.log(`server started successfully on port ${port} `);
+app.listen(port, async () => {
+    try{
+    await mongoose.connect(`mongodb://127.0.0.1/seeMeeApp`);
+    console.log("MongoDB connected successfully");
+    console.log(`server started successfully on port ${port} `);
+    }catch(error){
+        console.log("Error trying to connect to mongo");
+    }
 });
