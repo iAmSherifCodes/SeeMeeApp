d96800fe0303483cdf0d7834a3baa8073d5cc52c
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-10 20:24:22 +0100

    Imported functions from helper file

--- handlers/index.js
+++ handlers/index.js
@@ -1,10 +1,23 @@
-import { validateUserRegisterRequest, validateUserLoginRequest } from "../utils/helpers.js"
+import { validateUserRegisterRequest,
+         validateUserLoginRequest,
+         validateComment,
+         validateCreatePost,
+         validateFollowRequest,
+         validateLikePost,
+        validateViewNumberOfLikes }
+         from "../utils/helpers.js"
 import UserHandler from "./userHandler.js";
 import usecase from "../services/index.js"
 import ResponseHandler from "./response.js";
-import PostHandler from "./postHandler..js";
+import PostHandler from "./postHandler.js";
 
-const helpers = { validateUserLoginRequest, validateUserRegisterRequest};
+const helpers = { validateUserLoginRequest,
+                  validateUserRegisterRequest,
+                  validateComment,
+                  validateCreatePost,
+                  validateFollowRequest,
+                  validateLikePost,
+                validateViewNumberOfLikes};
 
 export default{
     userHandler: new UserHandler(usecase.userService, helpers, ResponseHandler),
