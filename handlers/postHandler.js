a737f9a10c20ef8fcb9e66241d1198e1c1a9d531
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-11 17:11:15 +0100

    Implemented methods in post handler

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -17,49 +17,46 @@ export default class PostHandler{
         try{
             const request = req.body;
 
-            const {error } = this._helpers.validateCreatePost(request);
+            const {error} = this._helpers.validateCreatePost(request);
             if(error){
                 return this._responseHandler.badRequest(res, error.message);
             }
 
-            if(req.file){
-
-                if (!req.file.originalname.match(/\.(JPG|jpg|jpeg|JPEG|PNG|png|mp4|MP4|mov|MOV|gif|GIF|avi|AVI)$/)) {
-                    return this._handler.badRequest(res, "Invalid Attachment Format");
-                }
-                if (req.fileValidationError) {
-                    return this._handler.badRequest(res, "Incorrect file type");
-                  }
-          
+            if(req.file){          
                   if (req.file && req.file.fieldname != "image" || req.file.fieldname != "video") {
                     return this._handler.badRequest(res, "Use image for image file key and video for video file key");
                   }
 
-                  // upload image or video to cloudinary and save the url to request.image
                   const cloudinary = require('cloudinary').v2;
                   cloudinary.config({
                     cloud_name: process.env.CLOUD_NAME,
                     api_key: process.env.API_KEY,
                     api_secret: process.env.API_SECRET
-                  }  
-                  );
+                  });
                   const file = req.file;
                   const result = await cloudinary.uploader.upload(file.path, {
                     resource_type: "auto",
                     folder: "seeMeeApp",
                     public_id: "social-media-app/" + file.filename,
                     overwrite: true,
-                    tags: "social-media-app"}
+                    tags: "social-media-app"}, (error, result) => {
+                        if (error) {
+                          console.error('Error uploading to Cloudinary:', error);
+                          return this._responseHandler.unprocessable(res, "Error uploading file")
+                        }
+                    }
                   );
-                  request.image = result.secure_url;
-                  request.image_id = result.public_id;
-                  request.image_type = result.resource_type;
-                  return this._responseHandler.created(res, request);
+                  console.log(result.resource_type, "resource type");
+                  if(req.file.fieldname === "image"){
+                    request.image = result.secure_url;
+                  }
+                  if(req.file.fieldname === "video"){
+                    request.video = result.secure_url;
+                  }
 
 
-
-
-                  // request.image = cloudinary upload url;
+                //   request.image_id = result.public_id;
+                //   request.image_type = result.resource_type;
             }
             const resp = await this._usecase.createPost(request);
             if (resp.success){
@@ -74,12 +71,14 @@ export default class PostHandler{
     async likePost (req, res){
         try{
             const request = req.body;
-            const {error } = this._helpers.validateLikePost(request);
+
+            const {error} = this._helpers.validateLikePost(request);
             if(error){
                 return this._responseHandler.badRequest(res, error.message);
             }
-            const resp = await this._usecase.likePost(request.postId, request.userId);
-            console.log(resp);
+            
+            const resp = await this._usecase.likePost(request.postId, request.userId, req.user.username);
+            // console.log(resp);
             if (resp.success){
                 return this._responseHandler.created(res, resp.data);
             }
@@ -92,11 +91,12 @@ export default class PostHandler{
     async commentPost (req, res){
         try{
             const request = req.body;
-            const {error } = this._helpers.validateCommentPost(request);
+            const {error} = this._helpers.validateComment(request);
             if(error){
                 return this._responseHandler.badRequest(res, error.message);
             };
-            const resp = await this._usecase.commentPost(request);
+            const resp = await this._usecase.commentPost(request.postId, request.userId, request.comment);
+            
             if (resp.success){
                 return this._responseHandler.created(res, resp.data);
             };
@@ -114,7 +114,7 @@ export default class PostHandler{
             if(error){
                 return this._responseHandler.badRequest(res, error.message);
             };
-            const resp = await this._usecase.viewNumberOfLikes();
+            const resp = await this._usecase.viewNumberOfLikes(request.postId);
             if (resp.success){
                 return this._responseHandler.created(res, resp.data);
             };
@@ -124,5 +124,21 @@ export default class PostHandler{
         }
     }
 
+    async viewComments(req, res){
+        try{
+            const request = req.body;
+            const {error} = this._helpers.validateViewComments(request);
+            if(error){
+                return this._responseHandler.badRequest(res, error.message);
+            };
+            const resp = await this._usecase.viewComments(request.postId);
+            if (resp.success){
+                return this._responseHandler.created(res, resp.data);
+            };
+            return this._responseHandler.badRequest(res, "Cannot view post of comments");
+        }catch(error){
+            return this._responseHandler.internalServerError(res, error.message || error);
+        }
+    }
   
 }
