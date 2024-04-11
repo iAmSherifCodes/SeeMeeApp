4ac66515a196399698a7c4cd85018fb21aa06d84
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-04-11 17:07:03 +0100

    Fixed service index

--- services/index.js
+++ services/index.js
@@ -1,9 +1,9 @@
 import UserService from "./user.js";
-import userRepo from "../repos/index.js";
+import repo from "../repos/index.js";
 import env from "../config/env.js";
 import PostService from "./post.js";
 
 export default {
-    userService: new UserService(userRepo, env()),
-    postService: new PostService(postRepo, env(), userRepo)
+    userService: new UserService(repo.userRepo, env()),
+    postService: new PostService(repo.postRepo, env(), repo.userRepo)
 }
